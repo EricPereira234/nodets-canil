0daa6bc756d82dbd190c83b65b5721f8278cc097
Eric Pereira <ericpereira234@gmail.com>
2023-09-28 07:54:49 -0300

    criando as pages

--- src/controllers/pageControllers.ts
+++ src/controllers/pageControllers.ts
@@ -1,17 +1,37 @@
 import { Request, Response } from "express";
 
 export const home = (req: Request, res: Response) => {
-    res.render('pages/page');
+    res.render('pages/page', {
+        banner: {
+            title: 'Todos os animais',
+            background: 'allanimals.jpg'
+        }
+    });
 }
 
 export const dogs = (req: Request, res: Response) => {
-    //res.render('pages/page');
+    res.render('pages/page', {
+        banner: {
+            title: 'Todos os Dogs',
+            background: 'banner_dog.jpg'
+        }
+    });
 }
 
 export const cats = (req: Request, res: Response) => {
-    //res.render('pages/page');
+    res.render('pages/page', {
+        banner: {
+            title: 'Todos os Gatos',
+            background: 'banner_cat.jpg'
+        }
+    });
 }
 
 export const fishes = (req: Request, res: Response) => {
-    //res.render('pages/page');
+    res.render('pages/page', {
+        banner: {
+            title: 'Todos os Fiches',
+            background: 'banner_fish.jpg'
+        }
+    });
 }
